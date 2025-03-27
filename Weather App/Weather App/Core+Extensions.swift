22438d4a61a6739934ae5e60ca32dbf479118860
williamdubgod <william_vulcano@hotmail.com>
2025-03-27 13:22:18 -0300

    Integração com API

---
+++ Weather App/Weather App/Core+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Core+Extensions.swift
+//  Weather App
+//
+//  Created by William Vulcano on 26/03/25.
+//
+
+import Foundation
---
+++ Weather App/Weather App/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  Weather App
+//
+//  Created by William Vulcano on 25/03/25.
+//
+
+import Foundation
